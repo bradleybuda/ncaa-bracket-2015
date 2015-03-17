7717b291e0a2cd045b89548c5507a571752a6907
Bradley Buda <bradleybuda@gmail.com>
2015-03-16 20:31:31 -0700

    Add victor prob formula

--- src/ncaa_bracket_2015/core.clj
+++ src/ncaa_bracket_2015/core.clj
@@ -1,6 +1,14 @@
 (ns ncaa-bracket-2015.core
   (:gen-class))
 
+(defn- pvictory [pythag-winner pythag-loser]
+  (let [a pythag-winner
+        b pythag-loser]
+    (/ (- a (* a b))
+       (+ a b (* -2 a b)))))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
