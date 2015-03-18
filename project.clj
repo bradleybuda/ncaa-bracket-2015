0bbc6bf3259f3fbb608ec34f073f4af45e83dd7e
Bradley Buda <bradleybuda@gmail.com>
2015-03-18 16:45:10 -0700

    Fix up dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject ncaa-bracket-2015 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/bradleybuda/ncaa-bracket-2015"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/math.combinatorics "0.1.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot ncaa-bracket-2015.core
   :target-path "target/%s"
