b0efbd96c8426567d0b7c51b129175fd98b35ca0
Bradley Buda <bradleybuda@gmail.com>
2015-03-18 16:27:00 -0700

    Potentially working cumulative EV calc - hard to be sure

--- src/ncaa_bracket_2015/core.clj
+++ src/ncaa_bracket_2015/core.clj
@@ -4,11 +4,12 @@
   (:require [clojure.core.typed :refer :all]
             [clojure.data.csv :as csv]
             [clojure.java.io :as io]
+            [clojure.math.numeric-tower :refer [expt]]
             [clojure.math.combinatorics :refer [cartesian-product permutations]])
 
   (:gen-class))
 
-(def final-four
+(def final-four-layout
   '(("Midwest" "West")
     ("East" "South")))
 
@@ -28,9 +29,12 @@
       (map
        (fn [record]
          (let [[_ name seed region kenpom] record]
-           ; TODO parse seed into numeric and play-in status
-           {:name name :seed (read-string (re-find #"\d+" seed)) :region region :kenpom (read-string kenpom)}
-           ))
+           {:name name
+            :seed (read-string (re-find #"\d+" seed))
+            :region region
+            :kenpom (read-string kenpom)
+            :play-in (re-matches #"\d+[ab]" seed)
+            }))
        (rest records)))))
 
 (def teams-and-seeds
@@ -64,7 +68,7 @@
    layout))
 
 (def full-bracket
-  (bracket final-four))
+  (bracket final-four-layout))
 
 (ann pvictory [Number Number -> Number])
 (defn- pvictory [pythag-winner pythag-loser]
@@ -73,36 +77,57 @@
     (/ (- a (* a b))
        (+ a b (* -2 a b)))))
 
+(defn- score-victory [round winner-seed loser-seed]
+  "How much a victory is worth given the round and relative seeds. Note
+   that round 0 is the 'opening round' played on Thursday / Friday - the
+   play-in games are round -1 and the national championship game is round 5."
+  (if (> 0 round)
+    0
+    (+  (expt 2 round)
+        (max 0 (- winner-seed loser-seed)))))
+
+(defn- score-outcome
+  "Determine the probability and value of a particular round outcome, i.e.
+  Team A defeats Team B in Round N."
+  [round-score-fn outcome]
+  (let [teams (map :team outcome)
+        winner (first teams)
+        win-probability (apply pvictory (map :kenpom teams))
+        score (apply round-score-fn (map :seed teams))
+        reach-probabilities (map :reach-probability outcome)
+        outcome-probability (apply * (cons win-probability reach-probabilities))
+        previous-score (:cumulative-ev (first outcome))
+        cumulative-ev (* outcome-probability (+ score previous-score))]
+    {:team winner :cumulative-ev cumulative-ev :reach-probability outcome-probability}))
 
 (defn- play-reach-distributions
   "Take a pair of reach-distributions and compute all outcomes of a game between them"
   [reach-distributions]
-  (let [distributions (map :reach-distribution reach-distributions)
+  (let [round (:round (first reach-distributions))
+        round-score-fn (partial score-victory round)
+        distributions (map :reach-distribution reach-distributions)
         ;; Pair off all the teams
         pairings (apply cartesian-product distributions)
         ;; Blow up into a list with both sets of outcomes for a pair (team A wins, team B wins)
         outcomes (mapcat permutations pairings)
         ;; Find the probability of each outcome based on the probability of the team reaching the round and their relative pyth
-        outcome-probabilities (map
-                               (fn [outcome]
-                                 (let [teams (map :team outcome)
-                                       win-probability (apply pvictory (map :kenpom teams))
-                                       reach-probabilities (map :reach-probability outcome)
-                                       outcome-probability (apply * (cons win-probability reach-probabilities))]
-                                   {:team (first teams) :reach-probability outcome-probability}))
-                               outcomes)
-
+        outcome-probabilities (map (partial score-outcome round-score-fn) outcomes)
         ;; Group the results by team and sum up
         outcomes-grouped-by-team (vals (group-by (comp :name :team) outcome-probabilities))
         reduced-outcomes (map (fn [team-group]
-                                (let [total-probability (apply + (map :reach-probability team-group))]
-                                  (assoc (first team-group) :reach-probability total-probability)))
+                                (let [total-probability (apply + (map :reach-probability team-group))
+                                      total-ev (apply + (map :cumulative-ev team-group))]
+                                  (assoc (first team-group)
+                                         :reach-probability total-probability
+                                         :cumulative-ev total-ev)))
                               outcomes-grouped-by-team)]
 
-    {:reach-distribution reduced-outcomes}))
+    {:round (inc round)
+     :reach-distribution reduced-outcomes}))
 
 (defn- make-single-team-reach-distribution [team]
-  {:reach-distribution (list {:team team :reach-probability 1.0})})
+  {:round (if (:play-in team) -1 0)
+   :reach-distribution (list {:team team :cumulative-ev 0.0 :reach-probability 1.0})})
 
 (defn- find-bracket-reach-distributions [sub-bracket]
   (if (seq? sub-bracket)
